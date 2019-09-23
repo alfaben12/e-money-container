7d4af59722c8f29331340708cedff9e29c5b2a4b
Thariqalfa <zrav420@gmail.com>
2019-09-23 13:33:42 +0700

    update & check mutation

--- controllers/EmoneyContainerController.js
+++ controllers/EmoneyContainerController.js
@@ -56,16 +56,6 @@ module.exports = {
 		let uuid = req.body.uuid;
 		let accountid = req.body.accountid;
 
-		  var post_data = 'nominal='+ nominal +'&payment_gateway_name='+ from_payment_gateway_name +"&uuid="+ uuid,
-		  headers = {
-			  host: 'bank.kutjur.com',
-			  method: 'POST',
-			  path: '/index.php/apis/mutations',
-			  headers: {
-				  'Content-Length': Buffer.byteLength(post_data)
-			  }
-		  };
-	  
 		let mutation_flag = false;
 		request.post('http://bank.kutjur.com/index.php/apis/mutations', {
 			form: {
@@ -75,7 +65,6 @@ module.exports = {
 		}, async function(error, response) {
 			mutation_flag = JSON.parse(response.body);
 			mutation_flag = mutation_flag.result;
-
 			if (mutation_flag == false) {
 				return res.status(200).json({
 					result : mutation_flag,
@@ -84,76 +73,96 @@ module.exports = {
 						message: "Mutation not found."
 					}
 			    });
-			}
-			/* FETCH ZSequelize */
-			let field = ['id', 'payment_gateway_containerid', 'payment_gateway_account_apikey', 'balance'];
-			let where = {
-				accountid: accountid
-			};
-			let orderBy = false;
-			let groupBy = false;
-			let model = 'AccountPaymentContainerModel'
-			let joins = [
-				[
-					{
-						'fromModel' : 'AccountPaymentContainerModel',
-						'fromKey' : 'payment_gateway_containerid',
-						'bridgeType' : 'belongsTo',
-						'toModel' : 'PaymentGatewayContainerModel',
-						'toKey' : 'id',
-						'attributes' : ['id', 'name'],
-						'required' : true
-					}
-				]
-			];
-			let container = await ZSequelize.fetchJoins(false, field, where, orderBy, groupBy, model, joins);
-			let to_payment_gateway_name = container.dataValues.payment_gateway_container.name;
-			let payment_gateway_account_apikey = container.dataValues.payment_gateway_account_apikey;
-			let balance = container.dataValues.balance;
-			
-			let insert = {
-			    accountid: accountid,
-			    from_payment_gateway_name: from_payment_gateway_name,
-			    to_payment_gateway_name: to_payment_gateway_name,
-			    nominal: nominal,
-				uuid: uuid,
-				is_transferred: 1
-			};
-			
-			let result_insert = await ZSequelize.insertValues(insert, "AccountPaymentHistoryModel");
-
-			let update = {
-			    balance: balance + nominal
-			};
-
-			/* UPDATE */
-			let where_container_account = {
-				accountid: accountid
-			};
-
-			/* UPDATE */
-			let where_container = {
-				api_key: payment_gateway_account_apikey
-			};
-
-			let result_container_account = await ZSequelize.updateValues(update, where_container_account, "AccountPaymentContainerModel");
-			let result_container = await ZSequelize.updateValues(update, where_container, "ApiPaymentGatewayAccountModel");
-
-			/* FETCTH RESULT & CONDITION & RESPONSE */
-			if (result_container.result) {
-			    return res.status(200).json({
-					result : result_container.result,
-					data:{
-						code: 200,
-						message: "Success move balance."
-					}
-			    });
 			}else{
-				return res.status(404).json({
-					result : result_container.result,
-					data:{
-						code: 404,
-						message: "Data not found."
+				request.post('http://bank.kutjur.com/index.php/apis/update_mutation', {
+					form: {
+						uuid: uuid
+					}
+				}, async function(error1, response1) {
+					mutation_update_flag = JSON.parse(response1.body);
+					mutation_update_flag = mutation_update_flag.result;
+
+					if (mutation_update_flag == false) {
+						return res.status(200).json({
+							result : mutation_update_flag,
+							data:{
+								code: 500,
+								message: "Server error."
+							}
+						});
+					}
+
+					/* FETCH ZSequelize */
+					let field = ['id', 'payment_gateway_containerid', 'payment_gateway_account_apikey', 'balance'];
+					let where = {
+						accountid: accountid
+					};
+					let orderBy = false;
+					let groupBy = false;
+					let model = 'AccountPaymentContainerModel'
+					let joins = [
+						[
+							{
+								'fromModel' : 'AccountPaymentContainerModel',
+								'fromKey' : 'payment_gateway_containerid',
+								'bridgeType' : 'belongsTo',
+								'toModel' : 'PaymentGatewayContainerModel',
+								'toKey' : 'id',
+								'attributes' : ['id', 'name'],
+								'required' : true
+							}
+						]
+					];
+					let container = await ZSequelize.fetchJoins(false, field, where, orderBy, groupBy, model, joins);
+					let to_payment_gateway_name = container.dataValues.payment_gateway_container.name;
+					let payment_gateway_account_apikey = container.dataValues.payment_gateway_account_apikey;
+					let balance = container.dataValues.balance;
+					
+					let insert = {
+						accountid: accountid,
+						from_payment_gateway_name: from_payment_gateway_name,
+						to_payment_gateway_name: to_payment_gateway_name,
+						nominal: nominal,
+						uuid: uuid,
+						is_transferred: 1
+					};
+					
+					let result_insert = await ZSequelize.insertValues(insert, "AccountPaymentHistoryModel");
+
+					let update = {
+						balance: balance + nominal
+					};
+
+					/* UPDATE */
+					let where_container_account = {
+						accountid: accountid
+					};
+
+					/* UPDATE */
+					let where_container = {
+						api_key: payment_gateway_account_apikey
+					};
+
+					let result_container_account = await ZSequelize.updateValues(update, where_container_account, "AccountPaymentContainerModel");
+					let result_container = await ZSequelize.updateValues(update, where_container, "ApiPaymentGatewayAccountModel");
+
+					/* FETCTH RESULT & CONDITION & RESPONSE */
+					if (result_container.result) {
+						return res.status(200).json({
+							result : result_container.result,
+							data:{
+								code: 200,
+								message: "Success move balance."
+							}
+						});
+					}else{
+						return res.status(404).json({
+							result : result_container.result,
+							data:{
+								code: 404,
+								message: "Data not found."
+							}
+						});
 					}
 				});
 			}
